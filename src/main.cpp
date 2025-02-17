c216d603a6396180f6ea060516bf094f32bc074f
PouPeuu <qwertydude277@gmail.com>
2025-02-17 12:55:52 +0200

    Create main

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,19 @@
 #include <iostream>
-#include <DissensionFramework/shader.hpp>
-#include <testipaska.hpp>
+#include <DissensionFramework/game.hpp>
+#include <main_game_object.hpp>
+
+using namespace DissensionFramework;
+using namespace GlibGame;
 
 int main() {
-    testkakka();
-    kakka();
+    printf("Hello from main\n");
+    Game game(800, 600, "Glib Game");
+    printf("game created\n");
 
-    /*lua_State* lua_state = luaL_newstate();
-    if (!lua_state) {
-        std::cerr << "Failed to create Lua state\n";
-        return 1;
-    }
+    MainGameObject* main_game_object = new MainGameObject();
+    game.addGameObject(main_game_object);
 
-    luaL_openlibs(lua_state);
-
-    std::string script((std::istreambuf_iterator<char>(std::cin)),
-                        std::istreambuf_iterator<char>());
-    
-    int load_status = luaL_loadbuffer(lua_state, script.c_str(), script.size(), "stdin");
-    if (load_status != LUA_OK) {
-        std::cerr << "Error loading script: " << lua_tostring(lua_state, -1) << std::endl;
-        lua_pop(lua_state, 1);
-        lua_close(lua_state);
-        return 1;
-    }
-
-    int exec_status = lua_pcall(lua_state, 0, LUA_MULTRET, 0);
-    if (exec_status != LUA_OK) {
-        std::cerr << "Error executing script: " << lua_tostring(lua_state, -1) << std::endl;
-        lua_pop(lua_state, 1);
-        lua_close(lua_state);
-        return 1;
-    }
-
-    lua_close(lua_state);*/
+    game.run();
 }
+
+// TODO: WON'T COMPIlE
